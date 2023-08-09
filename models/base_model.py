d4629354d7d2f9936c3c68f8ed20366c78f71677
MohammedBENHADINE <mohammedbenhadine@gmail.com>
2023-08-09 16:57:13 -0700

    add re creation based on dict

--- models/base_model.py
+++ models/base_model.py
@@ -23,11 +23,20 @@ class BaseModel:
         to_dict (self): returns a dictionary containing all keys/values of\
                 __dict__ of the instance
     """
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """instantiation time"""
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+        if not kwargs:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+        else:
+            for key, value in kwargs.items():
+                if (key != '__class__'):
+                    if key in ['created_at', 'updated_at']:
+                        setattr(self, key, datetime.strptime(
+                                value, '%Y-%m-%dT%H:%M:%S.%f'))
+                    else:
+                        setattr(self, key, value)
 
     def save(self):
         """Update the instance"""
